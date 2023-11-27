957f3d3e186d2538ecd8680a01c730ffd4f2ad30
lat0s <latosgeorge@gmail.com>
2023-11-27 15:57:41 +0100

    add navbar fix router

---
+++ app/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+<nav class="navbar sticky-top navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sticky top</a>
+  </div>
+</nav>
+</template>
+  
+
+
+<script>
+
+
+</script>
+
+
+<style>
+    
+</style>
