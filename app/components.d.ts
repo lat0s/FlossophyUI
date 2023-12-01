2e31b70327e0ea7d2b105ee28d97aee63c5f2d62
lat0s <latosgeorge@gmail.com>
2023-12-01 22:46:19 +0100

    Added Vue router + initial BookingView.vue file

--- app/components.d.ts
+++ app/components.d.ts
@@ -11,6 +11,7 @@ declare module 'vue' {
     BFormInput: typeof import('bootstrap-vue-next')['BFormInput']
     BModal: typeof import('bootstrap-vue-next')['BModal']
     BOffcanvas: typeof import('bootstrap-vue-next')['BOffcanvas']
+    BookingView: typeof import('./src/components/BookingView.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     MapView: typeof import('./src/components/MapView.vue')['default']
   }
