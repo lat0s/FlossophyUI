28ae9b80edc38d71f3f9fcdc88721f3204af0aac
lat0s <latosgeorge@gmail.com>
2023-12-06 01:24:59 +0100

    Add router

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+import router from './router'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-// Create app instance
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
